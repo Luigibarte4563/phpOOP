2416096a1ea0b1d1135f817a2d4074e4c80243aa
Luigibarte4563 <luigibarte1@gmail.com>
2025-08-26 22:12:39 +0800

    Add files via upload

---
+++ methodOverloading/calll.php
@@ -0,0 +1,24 @@
+<?php 
+    class call {
+        public function __call($name, $arguments) {
+            echo "Calling instance method '$name' with arguments";
+            print_r($arguments);
+            echo "<br>";
+        }
+
+        public static function __callStatic($name, $arguments) {
+            echo "Calling static method '$name' with arguments: ";
+            print_r($arguments);
+            echo "<br";
+        }
+    }
+
+    $obj = new call();
+
+    $obj->greet("Luigi", 21);
+
+    $obj->sum(10,10,10);
+
+    call::sayHello("World");
+
+    call::multiply(5,6);
