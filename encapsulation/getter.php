c182b532de507e8044cfe610b4b79c3a2b5bd9aa
Luigibarte4563 <luigibarte1@gmail.com>
2025-09-14 21:15:00 +0800

    Add files via upload

---
+++ encapsulation/getter.php
@@ -0,0 +1,17 @@
+<?php
+
+class Person {
+
+    private $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+}
+
+$person = new Person("Luigi");
+echo $person->getName();
