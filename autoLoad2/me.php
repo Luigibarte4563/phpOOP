d22a23bb4487bf97d9235dce7ea8433d6d9119f1
Luigi Barte <luigibarte1@gmail.com>
2025-10-05 21:59:09 +0800

    Add files via upload

---
+++ autoLoad2/me.php
@@ -0,0 +1,11 @@
+<?php
+
+class me {
+    public function __construct() {
+        echo "Hello! The class me has been loaded successfully! <br>";
+    }
+
+    public function greet() {
+        echo "Hello from TestClass!<br>";
+    }
+}
